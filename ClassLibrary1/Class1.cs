c32820238134a02f071ac9d39678d38b64ea8d99
Rodney Santiago <rodsan.724@outlook.com>
2022-02-04 12:42:00 -0500

    Added resuable library and WPF front entd

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+
+    #region Business Layer
+    public class BusinessLayer
+    {
+        public static List<Person> GetPeople()
+        {
+            List<Person> people = DataAccessLayer.GetPeople();
+
+            return people;
+        }
+    }
+    #endregion Business Layer
+
+    #region Data Layer
+    public class DataAccessLayer
+    {
+        //CRUD - Create, Read, Update and Delete records
+        public static List<Person> GetPeople()
+        {
+            List<Person> people = new List<Person>();
+            people.Add(new Person { Id = 1, FirstName = "Rodney" });
+            people.Add(new Person { Id = 2, FirstName = "Charity" });
+
+            return people;
+        }
+    }
+    #endregion Data Layer
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary1;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -24,37 +25,4 @@ namespace ConsoleApp1
         }
     }
     #endregion UI Layer
-
-    #region Business Layer
-    public class BusinessLayer
-    {
-        public static List<Person> GetPeople()
-        {
-            List<Person> people = DataAccessLayer.GetPeople();
-
-            return people;
-        }
-    }
-    #endregion Business Layer
-
-    #region Data Layer
-    public class DataAccessLayer
-    {
-        //CRUD - Create, Read, Update and Delete records
-        public static List<Person> GetPeople()
-        {
-            List<Person> people = new List<Person>();
-            people.Add(new Person { Id = 1, FirstName = "Rodney" });
-            people.Add(new Person { Id = 2, FirstName = "Charity" });
-            
-            return people;
-        }
-    }
-    #endregion Data Layer
-
-    public class Person
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-    }
 }
